20f70186b899c7901f88661ec528df443e9b1ad7
albert182219 <albert182219@gmail.com>
2024-11-25 01:43:40 +0000

    Firt commit

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+  class LoginPage extends Stateless Widget
+  const Login Page({super.key});
+  {
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold();
+  }
+}
